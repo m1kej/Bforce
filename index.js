5b6bf55e3d0c1abf8fea408e6d0fb508ab87e7c2
John Doe <darlionthesis@gmail.com>
2021-04-15 05:29:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
